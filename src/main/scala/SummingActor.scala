3456e5b8dd580d36371df6244b5486d1b779b24c
Sergey <drenozua@gmail.com>
2017-12-25 01:35:18 +0200

    actor whith constructor

--- src/main/scala/SummingActor.scala
+++ src/main/scala/SummingActor.scala
@@ -1,10 +1,10 @@
 import akka.actor.{Actor, ActorSystem, Props}
 
-class SummingActor extends Actor {
+class SummingActorWithConstructor(initialSum: Int) extends Actor {
   var sum = 0
 
   override def receive: Receive = {
-    case x: Int => sum += x
+    case x: Int => sum += initialSum + x
       println(s"my state as sum is $sum")
     case _ => println("I don't know what are you talking about")
   }
@@ -12,8 +12,7 @@ class SummingActor extends Actor {
 
 object BehaviourAndState extends App {
   val actorSystem = ActorSystem("HelloAkka")
-  var actor = actorSystem.actorOf(Props[SummingActor])
-  var actor2 = actorSystem.actorOf(Props[SummingActor], "actorName")
+  var actor = actorSystem.actorOf(Props(classOf[SummingActorWithConstructor], 10), "summingActor")
+
   println(actor.path)
-  println(actor2.path)
 }
